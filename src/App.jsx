5b1fbe075b9a9caa36cc926ded01e9d6fe9839d2
DipikaHalunde03 <dh071844@gmail.com>
2025-02-05 11:51:33 +0530

    add prblem statemets feature from admin side

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,29 @@ import Footer from "./component/footer";
 import EventRouting from "./component/Events/EventRouting";
 import Practice from "./component/Practice/Practice";
 import BootcampPage from "./component/Bootcamp/Boot";
+import AddProblemStatement from "./component/Problemstatement/AddPS";
 const App = () => {
   return (
     <Router>
       <div className="bg-white">
         <Header />
-        <Routes>
+        {/* <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/aboutus" element={<Aboutus />} />
           <Route path="/contactus" element={<Contactus />} />
           <Route path="/events/category/:category" element={<Home />} />
-          <Route path="/practice" element={<Practice />} />
+          <Route path="/practice" element={<Practice />} /> */}
           {/* <Route path="/Event_Management" element={<Event_Management />} />
           <Route path="/Add_New_Events" element={<Add_New_Events />} /> */}
-        </Routes>
+        {/* </Routes>
         <Student_Request />
         <Event_Management />
         <Add_New_Events />
-        <Footer />
+        <Footer /> */}
         {/* // this is bootcamp of admin dashboard */}
         {/* <BootcampPage/>      */}
          </div>
+        {/* < AddProblemStatement/>  // this page from admin dashboard*/}
     </Router>
   );
 };
